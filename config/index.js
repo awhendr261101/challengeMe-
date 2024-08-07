3b2cf2d314c7cc82601c1ef28a6a77d2af09b4f2
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-08-07 13:19:06 +0200

    dbconnection setup

--- config/index.js
+++ config/index.js
@@ -1,1 +1,30 @@
-//
+// dbconnection
+import { createPool } from "mysql2";
+import { config } from "dotenv";
+config();
+
+const e = process.env;
+
+const dbconnection = createPool(
+    {
+        host : e.HOSTDB,
+        user : e.USERDB,
+        password : e.DBPWD,
+        database : e.DBNAME,
+        multipleStatements : true,
+        connectionLimit : 30
+    }
+)
+
+dbconnection.on('connection', (pool)=>{
+    if (!pool) throw new Error("Oops, connection failed please try again later.")
+})
+
+dbconnection.on('error', (err)=>{
+    console.log('error connecting to database' + err.message)
+    process.exit(1)
+});
+
+export {
+    dbconnection
+}
