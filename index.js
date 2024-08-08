bec9a95271f45e342c690134b8aeefe143fcbfb5
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-08-08 12:21:40 +0200

    patch post and delete user

--- index.js
+++ index.js
@@ -180,6 +180,31 @@ app.get('/Product/:id',(req, res) => {
     }
 })
 
+app.patch('/Product/:id', bodyParser.json(), (req, res) => {
+    const { id } = req.params;
+    const { name, quantity, price, url, user } = req.body;
+
+    const priceInt = parseFloat(price);
+    const quantityInt = parseInt(quantity)
+    const idInt = parseInt(id);
+
+    try {
+        db.query(`
+        UPDATE Products
+        SET prodName ='${name}', prodQuantity='${quantityInt}', prodPrice='${priceInt}', prodURL='${url}', userID='${user}'
+        WHERE prodID= ${idInt}
+        `, (err, body) => {
+            if (err) throw new Error(err)
+            console.log('Product updated:', req.body);
+        });
+        console.log('Product updated:', req.body);
+        res.status(200).json({ message: 'Product updated successfully' });
+    } catch (err) {
+        console.error('ooops Something went wrong:', err.message);
+        res.status(500).json({ message: err.message });
+    }
+})
+
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
