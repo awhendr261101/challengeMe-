228d47e9d49a15f7ece0e61301973c07e2220cd5
awhendr261101 <awodwasihlehendrick@gmail.com>
2024-08-08 12:14:41 +0200

    patch post and delete user

--- index.js
+++ index.js
@@ -137,9 +137,6 @@ app.get('/products', (req, res) => {
     }
 })
 
-app.get('/addProduct', (req, res) =>{
-    res.status(200).sendFile(path.resolve('./static/html/register.html'))
-})
 
 app.post('/addProduct', bodyParser.json(),(req, res) => {
 
@@ -164,6 +161,22 @@ app.post('/addProduct', bodyParser.json(),(req, res) => {
     } catch (err) {
         console.error('Error registering product:', err.message);
         res.status(500).json({ message: err.message });
+    }
+})
+
+app.get('/Product/:id',(req, res) => {
+    const { id } = req.params;
+    try {
+        db.query(`SELECT * FROM Products WHERE prodID = ${id}`, (err, body) =>{
+            if(err) throw new Error(err)
+            res.status(200).json(body)
+        })
+    } catch (err) {
+        res.json(
+            {
+                message : err.message
+            }
+        )
     }
 })
 
